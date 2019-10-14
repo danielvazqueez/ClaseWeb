e4eaf1fb291cf00eeb31e62f4fbbfad388834129
danielvazqueez <luis_060198@hotmail.com>
2019-10-13 21:16:09 -0500

    Update lab 5

--- Lab5/app.js
+++ Lab5/app.js
@@ -2,37 +2,53 @@ const credentials = require('./credentials.js')
 
 const request = require('request')
 
-const mapBox = function(city) {
+const mapBox = function(city, callback) {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=${credentials.MAPBOX_TOKEN}`
   
   request({url, json: true}, (error, response) => {
     if (error) {
-      console.log(error)
+      callback(error, undefined)
     } else {
       const data = response.body
-      const longitud = data.features[0].center[0]
-      const latitud = data.features[0].center[1]
-      if (longitud) {
-        forecast(longitud, latitud)
+      if (response.statusCode != 200) {
+        callback(data.message, undefined)
+      } else {
+        if (!data.features.length) {
+          callback('No hay lugares relacionados', undefined)
+        } else {
+          const longitud = data.features[0].center[0]
+          const latitud = data.features[0].center[1]
+          forecast(longitud, latitud, callback) 
+        }
       }
     }
   })
 }
 
-const forecast = function(longitud, latitud) {
+const forecast = function(longitud, latitud, callback) {
   const url = `https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${latitud},${longitud}?lang=es&units=si`
 
   request({url , json: true}, (error, response) => {
     if (error) {
-      console.log(error)
+      callback(error, undefined)
     } else {
       const data = response.body;
-      const pronostico = `${data.hourly.summary} Actualmente esta a ${data.currently.temperature}°C, con una sensación térmica de ${data.currently.apparentTemperature}°C. Hay ${data.currently.precipProbability}% de posibilidad de lluvia.`
-      const pronostico2 = `Actualmente esta ${data.currently.summary.toLowerCase()} y para los siguientes dias se espera ${data.daily.summary}`
-      console.log(pronostico)
-      console.log(pronostico2)
+      if (data.code) {
+        callback(data.error, undefined)
+      } else {
+        const pronostico = `${data.hourly.summary} Actualmente esta a ${data.currently.temperature}°C, con una sensación térmica de ${data.currently.apparentTemperature}°C. Hay ${data.currently.precipProbability}% de posibilidad de lluvia.`
+        const pronostico2 = `Actualmente esta ${data.currently.summary.toLowerCase()} y para los siguientes dias se espera ${data.daily.summary}`
+        callback(undefined, pronostico)
+        callback(undefined, pronostico2)
+      }
     }
   })
 }
 
-mapBox('Monterrey')
+mapBox('Monterrey', (error, data) => {
+  if (error) {
+    console.log('Error: ', error)
+  } else {
+    console.log(data)
+  }
+})
